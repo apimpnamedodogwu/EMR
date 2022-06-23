86f685aec4e93fb5bd8e9b449696f744e22a7c48
quadrii <adisaquadri234@yahoo.com>
2022-06-23 13:52:40 +0100

    working on driver class

--- src/EmrDriverClass.java
+++ src/EmrDriverClass.java
@@ -1,5 +1,17 @@
+import emr_project.Hospital;
+
+import java.util.Scanner;
+
 public class EmrDriverClass {
     public static void main(String[] args) {
-        
+        String hospitalName;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Hospital Name");
+        hospitalName = input.next();
+        Hospital hospital = new Hospital();
+        hospital.setHospitalName(hospitalName);
+
+        System.out.println("Welcome to " + hospital.getHospitalName());
     }
+
 }
